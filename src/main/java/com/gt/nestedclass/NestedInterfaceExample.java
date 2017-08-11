ced792d31b032c975295857e81e95813d2d4dc25
Gaurav <gauravtiwari9098@gmail.com>
2017-08-11 16:35:35 +0530

    updated code

--- src/main/java/com/gt/nestedclass/NestedInterfaceExample.java
+++ src/main/java/com/gt/nestedclass/NestedInterfaceExample.java
@@ -8,13 +8,23 @@ interface Showable {
 	}
 }
 
-public class NestedInterfaceExample implements Showable.Message {
+public class NestedInterfaceExample implements Showable.Message ,Showable {
 	public void msg() {
 		System.out.println("Hello nested interface");
 	}
-
+	@Override
+	public void show() {
+		System.out.println("Hello Outer interface");
+		
+	}
 	public static void main(String args[]) {
 		Showable.Message message = new NestedInterfaceExample();// upcasting  here
 		message.msg();
+		
+		
+		Showable show = new NestedInterfaceExample();
+		show.show();
 	}
+
+
 }
